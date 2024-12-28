b0f84e5a6c8bd70ee5f665d30a3b63ac14c50d25
Ashu Sharma <ashusharma@Ashus-MacBook-Air.local>
2024-12-28 16:29:41 +0530

    Arithmetic Operator

---
+++ ch2_Operators/arithmeticOperator.js
@@ -0,0 +1,11 @@
+//Arithmetic Operator
+
+let num1 = 20;
+let num2 = 15;
+
+console.log(num1 + num2); //add
+console.log(num1 - num2); //sub
+console.log(num1 * num2); //Multiplication
+console.log(num1 / num2); //division
+console.log(num1 % num2); //Reminder |Mod|
+console.log(num1 ** num2);// e to the power x
---
+++ ch2_Operators/assignmentOperator.js
@@ -0,0 +1,29 @@
+// Assignment Operator
+
+let num1 = 10;
+let num2 = num1;
+console.log(num2);
+
+num1 +=5;
+console.log(num1);
+
+num1 = num1 + 10;
+
+console.log(num1);
+
+num1 = num1 -5;
+num1 -= 5;
+
+
+num1 = num1 * 5;
+num1 *= 5;
+
+
+num1 = num1 + 1;
+num1 +=1;
+num1++;
+
+num1 = num1 - 1;
+num1 -= 1;
+num1--;
+
