44488bbfac9043ddc340a593fa78ac10bce6027c
suitecrmdevashu <phpdevashu@gmail.com>
2025-01-23 18:21:33 +0530

    substring

---
+++ ch5_String/stringSubstring.js
@@ -0,0 +1,8 @@
+/**
+ * The slice() method extracts a part of a string.
+ * The slice() method returns the extracted part in a new string.
+ * The slice() method does not change the original string.
+ * The start and end parameters specifies the part of the string to extract.
+ * The first position is 0, the second is 1, ...
+ * A negative number selects from the end of the string.
+ */
